b70de9e9b58291275e06f046b8c87c1f9ffd724c
Andrew Ellison <ellisonc@wustl.edu>
2016-02-24 16:22:50 -0600

    html file serv

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
-var http = require("http");
+var http = require("http"),
+url = require('url'),
+path = require('path'),
+mime = require('mime'),
+fs = require('fs');
+
+function sendFile(error, data){
+	if(err){
+		response.writeHead(404, {"Content-Type" : "text/plain"});
+		response.write("Internal server error: could not read file");
+		response.end();
+		return;
+	}
+	
+	var mimetype = mime.lookup(filename);
+	response.writeHead(200,{"Content-Type" : mimetype});
+	response.write(data);
+	response.end();
+	return;
+}
 
 function serve(request, response){
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello World");
-	response.end();
+	var filename = "app.html";
+	fs.readFile(filename, sendFile);
 }
 
 var server = http.createServer(serve);
