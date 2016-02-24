d63937a98ad6af6820abd9ee748316ea68456d24
Andrew Ellison <ellisonc@wustl.edu>
2016-02-24 15:33:17 -0600

    server
    simple http server

---
+++ server.js
@@ -0,0 +1,6 @@
+var http = requrie("http");
+http.createServer(function(request, response) {
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello World");
+	response.end();
+}).listen(8888);
