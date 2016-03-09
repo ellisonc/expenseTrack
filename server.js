fc872b31c790f18ff26657c8f6d7b858fd235fe2
Andrew Ellison <ellisonc@wustl.edu>
2016-03-08 18:45:14 -0600

    again

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var users = {};
 
 http.listen(8888);
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + 'app.html');
+    res.sendFile(__dirname + '/expenseTrack/files/app.html');
 })
 
 
