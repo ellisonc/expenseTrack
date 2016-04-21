6494563dae682ebf14802a2aff89d96fcd6f49ba
Andrew Ellison <ellisonc@wustl.edu>
2016-04-20 22:50:08 -0500

    maybe a typo

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ MongoClient.connect(url, function (err, db) {
         socket.on('check', function (userName, fn) {
             console.log('checking ' + userName);            
             //query mongodb
-            var count = db.collection('users').find({ "userName": userName }).limit(1).count();
+            var count = db.collection('users').find({ "username": userName }).limit(1).count();
             console.log(count);
             if (count == 1) {
                 fn(true);
