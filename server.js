a2477178cbaeb9172d948b72307d4187cf44f570
Andrew Ellison <ellisonc@wustl.edu>
2016-04-20 23:36:57 -0500

    mongoose

--- server.js
+++ server.js
@@ -5,60 +5,118 @@ var express = require('express'),
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var url = 'mongodb://localhost:27017/expenseTrack';
+var mongoose = require('mongoose');
 
-MongoClient.connect(url, function (err, db) {
-    assert.equal(null, err);
-    console.log("Connected to db");
-    http.listen(8888);
-    app.get('/', function (req, res) {
-        res.sendFile(__dirname + '/files/app.html');
+var db = mongoose.connection;
+
+db.on('error', console.error);
+db.once('open', function () {
+    var userSchema = new mongoose.Schema({
+        username: String,
+        password: String,
+        name: String,
+        rooms: Array
     });
 
-    app.use(express.static('files'));
+    var roomSchema = new mongoose.Schema({
+        roomID: String,
+        users: Array,
+        Items: Array
+    });
+
+    var itemSchema = new mongoos.Schema({
+        itemID: Number,
+        amount: Number,
+        description: String,
+        user: String,
+        room: String,
+        timeStamp: Date
+    });
+
+    var User = mongoose.model('User', userSchema);
+    var Room = mongoose.model('Room', roomSchema);
+    var Item = mongoose.model('Item', itemSchema);
+});
+
+mongoose.connect(url);
+
+io.on('connection', function (socket) {
+
+    socket.on('newUser', function (data) {
+        var tempUser = new User({
+            username: data.username,
+            password: data.password,
+            name: data.name,
+            rooms: null
+        });
+        tempUser.save(function (err, tempUser) {
+            if (err) return console.error(err);
+        });
+    });
+
+    socket.on('check', function (username, fn) {
+        User.count({ username: username }, function (err, count) {
+            if (count == 0) {
+                fn(false);
+            }
+            else {
+                fn(true);
+            }
+        })
+    });
+});
+
+//MongoClient.connect(url, function (err, db) {
+//    assert.equal(null, err);
+//    console.log("Connected to db");
+//    http.listen(8888);
+//    app.get('/', function (req, res) {
+//        res.sendFile(__dirname + '/files/app.html');
+//    });
+
+//    app.use(express.static('files'));
     
 
-    io.on('connection', function (socket) {
-        db.collection('users').insertOne({ "username": "tester" });
+//    io.on('connection', function (socket) {
+//        socket.on('check', function (userName, fn) {
+//            console.log('checking ' + userName);            
+//            //query mongodb
+//            var cursor = db.collection('users').find({ "username": userName });
+//            cursor.count(function (err, count) {
+//                console.log(count);
+//                if (count != 0) {
+//                    fn(true);
+//                }
+//                else {
+//                    fn(false);
+//                }
+//            });
+//        });
 
-        socket.on('check', function (userName, fn) {
-            console.log('checking ' + userName);            
-            //query mongodb
-            var cursor = db.collection('users').find({ "username": userName });
-            cursor.count(function (err, count) {
-                console.log(count);
-                if (count != 0) {
-                    fn(true);
-                }
-                else {
-                    fn(false);
-                }
-            });
-        });
+//        socket.on('addItem', function (item) {
+//            console.log('addItem: ');
+//            console.log('type - ' + item.type);
+//            console.log('creatorID - ' + item.creatorID);
+//            console.log('date - ' + item.date);
+//            console.log('description - ' + item.description);
+//        });
 
-        socket.on('addItem', function (item) {
-            console.log('addItem: ');
-            console.log('type - ' + item.type);
-            console.log('creatorID - ' + item.creatorID);
-            console.log('date - ' + item.date);
-            console.log('description - ' + item.description);
-        });
+//        socket.on('delete', function (target) {
+//            console.log('target: ');
+//            console.log(target);
+//        });
 
-        socket.on('delete', function (target) {
-            console.log('target: ');
-            console.log(target);
-        });
+//        socket.on('newUser', function (userName) {
+//            //db.users.insert({UserName: userName})
+//        });
 
-        socket.on('newUser', function (userName) {
-            //db.users.insert({UserName: userName})
-        });
-
-        socket.on('disconnect', function () {
-            console.log('disconnected');
-        });
-    });
+//        socket.on('disconnect', function () {
+//            console.log('disconnected');
+//        });
+//    });
 
 
 
-    db.close();
-});
+//    db.close();
+//});
 
